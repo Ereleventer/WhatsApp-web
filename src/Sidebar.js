cfc79ec28c19de8b1e8404ebde815020478e7f7c
ereleventer <erel@optibus.com>
2022-04-10 13:44:12 +0300

    chat

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,17 +6,31 @@ import SidebarChat from "./SidebarChat";
 
 
 function Sidebar(){
+
+
+    const createChat = () => {
+        const roomName = prompt("Please enter name for chat");
+
+        if (roomName) {
+            // login to add new chat
+        }
+    };
+
+
   return (
   <div className ="sidebar"> 
     <div className="sidebar_header">
         <img src={avatar} className="avatar" />
         <div className="sidebar_headerRight">
-            <button data-toggle="tooltip" data-placement="top" title="Create a New Chat" className="chatIconButton"><img src={chatIcon} className="chatIcon" /></button>
+            <button onClick={createChat} data-toggle="tooltip" data-placement="top" title="Create a New Chat" className="chatIconButton"><img src={chatIcon} className="chatIcon" /></button>
 
         </div>
     </div>
     <div className="sidebar_chats">
         <SidebarChat/>
+        <SidebarChat/>
+        <SidebarChat/>
+
     </div>
   </div>
   );
