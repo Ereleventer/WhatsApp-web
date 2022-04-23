465634519179bab0749ae5b8d033b79d7aef2dbe
ereleventer <erel@optibus.com>
2022-04-23 12:18:45 +0300

    change

--- src/data/newContantMessages.js
+++ src/data/newContantMessages.js
@@ -3,10 +3,12 @@ export let messages = [
       content: "Welcome to our chat!",
       sender: "newChatName",
       time: "20:12",
+      type: "text", 
     },
     {
       content: "Welcome!!",
       sender: "newChatName",
       time: "23:50",
+      type: "text",
     }
 ]
