9028863182933fe4829c4b5f87d2e8db0504a4f8
ereleventer <erel@optibus.com>
2022-04-28 20:48:15 +0300

    fix regi bug

--- src/localDataBase.js
+++ src/localDataBase.js
@@ -9,6 +9,8 @@ export let Registered_Users = new Array(
     password: "7894",
     nickname: "Yotam",
     pic: avatar6,
+    isNew: false
+
   },
 
   {
@@ -16,6 +18,8 @@ export let Registered_Users = new Array(
     password: "5567",
     nickname: "Rotem",
     pic: avatar7,
+    isNew: false
+
   },
 
   {
@@ -23,17 +27,22 @@ export let Registered_Users = new Array(
     password: "abcd",
     nickname: "David",
     pic: avatar8,
+    isNew: false
   },
   {
     username: "eytan99",
     password: "1654",
     nickname: "Eytan",
     pic: avatar8,
+    isNew: false
+
   },
   {
     username: "test",
     password: "12",
     nickname: "Test",
     pic: avatar9,
+    isNew: false
+
   }
 );
